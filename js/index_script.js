e2d3778e96b9aa457e8593fce79c4348ada9cea0
Tanner Mills <tanner.mills2000@gmail.com>
2023-09-10 16:21:21 -0600

    Made a class for todo items

--- js/index_script.js
+++ js/index_script.js
@@ -1,3 +1,20 @@
+class TodoItem {
+	/* 
+	 * List is being added to constructor params to allow for the future addition of allowing
+	 * multiple todo lists.
+	 */
+
+	constructor(text, container) {
+		this.item = document.createElement('li');
+		this.item.textContent = text;
+		
+		this.container = container;
+		this.container.appendChild(this.item);	
+	}
+
+
+}
+
 window.onload = (event) => {
 	console.log("Page is fully loaded.");
 	const todoButton = document.querySelector('button#todoButton');
@@ -9,9 +26,9 @@ window.onload = (event) => {
 		console.log("Input set to: " + input.value);
 		if (input.value) {
 			const todoList = document.querySelector("#todoList");
-			const newTodo = document.createElement('li');
-			newTodo.textContent = input.value;
-			todoList.appendChild(newTodo);
+			
+			const newTodo = new TodoItem(input.value, todoList);
+
 			input.value = "";
 		}
 
