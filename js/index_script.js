2e000b2259bb4e3b6528eaaafdb8436c3e31da4b
Tanner Mills <tanner.mills2000@gmail.com>
2023-09-27 09:46:06 -0600

    JS Code cleanup. Created two new private methods in Task class for creating the child nodes.

--- js/index_script.js
+++ js/index_script.js
@@ -62,37 +62,14 @@ class Task {
 
 	constructor(text, taskList) {
 
+		// Assign the parent Node (Task List Object)
 		this.#taskList = taskList;
 
-		this.#title = document.createElement('p');
-		this.#title.setAttribute('class', 'todo-title');
-		this.setText(text);
+		// Create the HTML elements
+		this.#createHeader();
+		this.#createDrawer();
 
-		this.#checkBox = document.createElement('input');
-		this.#checkBox.setAttribute('type', 'checkbox');
-		this.#checkBox.setAttribute('class', 'todo-checkbox');
-		this.#checkBox.addEventListener('click', () => this.#todoChecked(this.#checkBox.checked));
-
-		this.#deleteBtn = document.createElement('button');
-		this.#deleteBtn.setAttribute('type', 'button');
-		this.#deleteBtn.setAttribute('class', 'task-delete-button');
-		this.#deleteBtn.addEventListener('click', () => this.#deleteClicked());
-
-		this.#drawerToggle = document.createElement('button');
-		this.#drawerToggle.setAttribute('type', 'button');
-		this.#drawerToggle.setAttribute('class', 'expand-drawer-button');
-		this.#drawerToggle.addEventListener('click', () => this.#toggleDrawer());
-
-		this.#taskHeader = document.createElement('div');
-		this.#taskHeader.setAttribute('class', 'task-header');
-		this.#taskHeader.appendChild(this.#checkBox);
-		this.#taskHeader.appendChild(this.#title);
-		this.#taskHeader.appendChild(this.#drawerToggle);
-	
-		this.#drawer = document.createElement('div');
-		this.#drawer.setAttribute('class', 'task-drawer');
-		this.#drawer.appendChild(this.#deleteBtn);
-
+		// Create a wrapper div to house all the elements and assign the children.
 		this.#taskWrapper = document.createElement('div');
 		this.#taskWrapper.setAttribute('class', 'task-content-wrapper');
 		this.#taskWrapper.appendChild(this.#taskHeader);
@@ -129,6 +106,39 @@ class Task {
 
 	#deleteClicked() {
 		this.#taskList.deleteTask(this);
+	}
+
+	#createHeader() {
+		this.#title = document.createElement('p');
+		this.#title.setAttribute('class', 'todo-title');
+		this.setText(text);
+
+		this.#checkBox = document.createElement('input');
+		this.#checkBox.setAttribute('type', 'checkbox');
+		this.#checkBox.setAttribute('class', 'todo-checkbox');
+		this.#checkBox.addEventListener('click', () => this.#todoChecked(this.#checkBox.checked));
+
+		this.#drawerToggle = document.createElement('button');
+		this.#drawerToggle.setAttribute('type', 'button');
+		this.#drawerToggle.setAttribute('class', 'expand-drawer-button');
+		this.#drawerToggle.addEventListener('click', () => this.#toggleDrawer());
+
+		this.#taskHeader = document.createElement('div');
+		this.#taskHeader.setAttribute('class', 'task-header');
+		this.#taskHeader.appendChild(this.#checkBox);
+		this.#taskHeader.appendChild(this.#title);
+		this.#taskHeader.appendChild(this.#drawerToggle);
+	}
+
+	#createDrawer() {
+		this.#deleteBtn = document.createElement('button');
+		this.#deleteBtn.setAttribute('type', 'button');
+		this.#deleteBtn.setAttribute('class', 'task-delete-button');
+		this.#deleteBtn.addEventListener('click', () => this.#deleteClicked());
+
+		this.#drawer = document.createElement('div');
+		this.#drawer.setAttribute('class', 'task-drawer');
+		this.#drawer.appendChild(this.#deleteBtn);
 	}
 
 	#toggleDrawer() {
